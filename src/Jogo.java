bcd6098e5af367ffbccf31c19b22b453b181fc00
Dione <dionebrasilr@gmail.com>
2026-06-16 14:57:21 -0300

    .

--- src/Jogo.java
+++ src/Jogo.java
@@ -1,2 +1,7 @@
 public class Jogo {
+    //Classe Jogo:
+    //Responsável por controlar toda a lógica da partida, coordenando a interação entre jogador, tabuleiro, movimentos e ranking.
+    //Retorna: Resultado, pontuação.
+
+
 }
