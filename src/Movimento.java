25923b80fa28ba3a72a04b050378b8acf2a3c964
Dione0 <dionebrasilr@gmail.com>
2026-06-19 21:01:05 -0300

    Wrap mover in Movimento class and add verificar
    Convert the top-level mover function into a Movimento class method (renamed mover -> Mover), reorganize indentation, and encapsulate the movement logic. Add a switch to compute the target cell based on input ('w','a','s','d' with uppercase variants), validate bounds, perform the tile swap, and return status codes. Also add a stub verificar(Tabuleiro, int) returning false for future validation logic.

--- src/Movimento.java
+++ src/Movimento.java
@@ -1,47 +1,53 @@
-public int mover(int[][] matriz, int n, char movimento) {
-    int linhaVazia = 0;
-    int colunaVazia = 0;
+public class Movimento {
+    public int Mover(int[][] matriz, int n, char movimento) {
+        int linhaVazia = 0;
+        int colunaVazia = 0;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (matriz[i][j] == 0) {
-                linhaVazia = i;
-                colunaVazia = j;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matriz[i][j] == 0) {
+                    linhaVazia = i;
+                    colunaVazia = j;
+                }
             }
         }
-    }
 
-    int novaLinha = linhaVazia;
-    int novaColuna = colunaVazia;
+        int novaLinha = linhaVazia;
+        int novaColuna = colunaVazia;
 
-    switch (movimento) {
-        case 'w':
-        case 'W':
-            novaLinha++;
-            break;
-        case 's':
-        case 'S':
-            novaLinha--;
-            break;
-        case 'a':
-        case 'A':
-            novaColuna++;
-            break;
-        case 'd':
-        case 'D':
-            novaColuna--;
-            break;
-        default:
+        switch (movimento) {
+            case 'w':
+            case 'W':
+                novaLinha++;
+                break;
+            case 's':
+            case 'S':
+                novaLinha--;
+                break;
+            case 'a':
+            case 'A':
+                novaColuna++;
+                break;
+            case 'd':
+            case 'D':
+                novaColuna--;
+                break;
+            default:
+                return 0;
+        }
+
+        if (novaLinha < 0 || novaLinha >= n || novaColuna < 0 || novaColuna >= n) {
             return 0;
+        }
+
+        int temp = matriz[novaLinha][novaColuna];
+        matriz[novaLinha][novaColuna] = 0;
+        matriz[linhaVazia][colunaVazia] = temp;
+
+        return 1;
     }
 
-    if (novaLinha < 0 || novaLinha >= n || novaColuna < 0 || novaColuna >= n) {
-        return 0;
+    public boolean verificar(Tabuleiro tabuleiro, int peca) {
+        return false;
     }
-
-    int temp = matriz[novaLinha][novaColuna];
-    matriz[novaLinha][novaColuna] = 0;
-    matriz[linhaVazia][colunaVazia] = temp;
-
-    return 1;
 }
